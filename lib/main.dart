a28e034e8aa0e124cc18d95d35616fceea377cea
Rachana A <rachana.aravindan@gmail.com>
2021-11-11 12:20:36 +0530

    rachana

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:ashok_leyland_project_3/screens/select_department_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-
 void main() {
   runApp(MyApp());
 }
@@ -11,15 +9,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false ,
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        textTheme: GoogleFonts.poppinsTextTheme(
-          Theme.of(context).textTheme,
-        ),
-        accentColor: Color(0xFFFF1E00)
-      ),
+          textTheme: GoogleFonts.poppinsTextTheme(
+            Theme.of(context).textTheme,
+          ),
+          accentColor: Color(0xFFFF1E00)),
       title: 'Ashok_leyland',
-     
       home: SelectDepartmentScreen(),
     );
   }
